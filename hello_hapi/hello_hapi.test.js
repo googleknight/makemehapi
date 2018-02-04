133c474531bc04a80cd67be5465ddcd6a2821979
Shubham Mathur <shubham.mathur@engineer.com>
2018-02-04 20:29:52 +0530

    hello_hapi - Updated test cases for inject

--- hello_hapi/hello_hapi.test.js
+++ hello_hapi/hello_hapi.test.js
@@ -1,26 +1,22 @@
 const server = require('./hello_hapi');
-const request = require('request');
 
 describe('The hapi server started should return a message on get request', () => {
-  beforeAll((done) => {
-    server.start(() => {
-      console.log('Server started');
-      done();
-    });
-  });
-  afterAll((done) => {
-    server.stop(() => {
-      done();
-    });
-  });
   it('Checking the string returned from a get request to / path to server', (done) => {
-    request('http://localhost:8080', (error, response, body) => {
-      expect(body).toBe('Hello hapi');
+    const request = {
+      method: 'GET',
+      url: '/',
+    };
+    server.inject(request, (response) => {
+      expect(response.result).toBe('Hello hapi');
       done();
     });
   });
   it('Checking the response code of the response', (done) => {
-    request('http://localhost:8080', (error, response, body) => {
+    const request = {
+      method: 'GET',
+      url: '/',
+    };
+    server.inject(request, (response) => {
       expect(response.statusCode).toBe(200);
       done();
     });
